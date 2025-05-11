ed3eff0565d0c4bd6f543a951fa558e8c237a259
MauriceElliott <mauricemelliott@gmail.com>
2025-05-11 23:29:21 +0100

    starting again

--- Core/.swiftpm/build-and-run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-set -e
-killall 'Playdate Simulator' || true
-cd ..
-PRODUCT=$(sed -n '/^PRODUCT :=/s///p' Makefile)
-make 
-~/Developer/PlaydateSDK/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator $PRODUCT
