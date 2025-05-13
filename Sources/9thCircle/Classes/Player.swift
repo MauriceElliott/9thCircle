5b0d32d65a8efe80230ec13a8ddb2b132400583a
MauriceElliott <mauricemelliott@gmail.com>
2025-05-13 23:07:18 +0100

    Initial commit of player and debugging

--- Sources/9thCircle/Classes/Player.swift
+++ Sources/9thCircle/Classes/Player.swift
@@ -1,17 +1,83 @@
 import PlaydateKit
 
 class Player: Sprite.Sprite {
-    // MARK: Lifecycle
+    var velocity: (dx: Float, dy: Float)
+    
+    //Vars
+    var isOnGround: Bool = false
+    var isJumping: Bool = false
+
+    //Movement Statics - These define params for movement, would be awesome to make them adjustable at some point.
+    let moveSpeed: Float = 100.0
+    let jumpForce: Float = 450.0
+    let gravity: Float = 100.0
+    let maxFallSpeed: Float = 600.0
+    let airControlFactor: Float = 0.8
+    
+    //Debug values
+    let groundLevelY: Float = 200
 
     override init() {
+        velocity = (dx: 0, dy: 0)
         super.init()
-        image = try! Graphics.Bitmap(path: "logo.png")
-        bounds = .init(x: 0, y: 0, width: 400, height: 240)
+        position.y = groundLevelY
+        image = try! Graphics.Bitmap(path: "test.png")
+        bounds = .init(x: 0, y: 0, width: 20, height: 40)
+        if position.y >= groundLevelY {
+            position.y = groundLevelY
+            isOnGround = true
+        }
     }
 
     // MARK: Internal
 
     override func update() {
-        moveBy(dx: 0, dy: sinf(System.elapsedTime * 4))
+        //Super accurate DeltaTime
+        let dt = System.elapsedTime
+        System.resetElapsedTime()
+        
+        let currentSpeed = isOnGround ? moveSpeed : moveSpeed * airControlFactor
+
+        if System.buttonState.current == PDButtons.left {
+            velocity.dx = -currentSpeed
+        } else if System.buttonState.current == PDButtons.right {
+            velocity.dx = currentSpeed
+        } else {
+            velocity.dx = 0
+        }
+        
+        //Jump TODO: needs to moved out to game state rather than specifically checking the buttons here.
+        if System.buttonState.current == PDButtons.a && isOnGround {
+            isJumping = true
+            velocity.dy = -jumpForce
+        }
+        
+        //Apply Gravity
+        if !isOnGround {
+            velocity.dy += gravity * dt
+            if velocity.dy > maxFallSpeed {
+                velocity.dy = maxFallSpeed
+            }
+        }
+        
+        position.x += velocity.dx * dt
+        position.y += velocity.dy * dt
+        
+        if position.y >= groundLevelY {
+            if velocity.dy > 0 {
+                position.y = groundLevelY
+                velocity.dy = 0
+                isOnGround = true
+                isJumping = false
+            }
+        }
+
+        if position.y < groundLevelY && !isJumping {
+            isOnGround = false
+        }
+        
+        let xAsInt = Int(position.x)
+        let yAsInt = Int(position.y)
+        print("Position x (approx): \(xAsInt), y (approx): \(yAsInt)")
     }
 }
