5caa5f118de9c8c5fa196fcafe97395f7ab69cd8
MauriceElliott <mauricemelliott@gmail.com>
2025-05-13 21:46:48 +0100

    debugging

--- Sources/9thCircle/Game.swift
+++ Sources/9thCircle/Game.swift
@@ -23,21 +23,3 @@ final class Game: PlaydateGame {
         print("Paused!")
     }
 }
-
-// MARK: - Logo
-
-class Logo: Sprite.Sprite {
-    // MARK: Lifecycle
-
-    override init() {
-        super.init()
-        image = try! Graphics.Bitmap(path: "logo.png")
-        bounds = .init(x: 0, y: 0, width: 400, height: 240)
-    }
-
-    // MARK: Internal
-
-    override func update() {
-        moveBy(dx: 0, dy: sinf(System.elapsedTime * 4))
-    }
-}
