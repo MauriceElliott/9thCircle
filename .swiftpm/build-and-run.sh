ee123919f84ab09ce0f45f5c6378c4f6670ccab8
MauriceElliott <mauricemelliott@gmail.com>
2025-05-18 21:22:02 +0100

    debugging

--- .swiftpm/build-and-run.sh
+++ .swiftpm/build-and-run.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
 set -e
-
+echo "debug1"
 cleanup() {
     osascript -e 'quit app "Playdate Simulator"' 2>/dev/null
 }
 
+echo "debug2"
 trap cleanup EXIT
 
 cd ..
 swift package pdc
 
+echo "debug3"
 # Create a symbolic link to the compiled pdx in the Playdate Simulator Games dir
 # This allows browsing the pdx as a sideloaded game, helpful for checking icons
 if [ -e ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx ]
@@ -17,5 +19,7 @@ then
     rm -rf ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
 fi
 ln -s "$(pwd)/.build/plugins/PDCPlugin/outputs/$CONFIGURATION/$PRODUCT_NAME.pdx" ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
+
+echo "debug4"
 
 ~/Developer/PlaydateSDK/bin/Playdate\ Simulator.app/Contents/MacOS/Playdate\ Simulator ~/Developer/PlaydateSDK/Disk/Games/$PRODUCT_NAME.pdx
